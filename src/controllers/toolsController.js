d3337768199802620ac9da34a584f6939fd555c0
bkmoises <bk_moises@hotmail.com>
2023-08-27 18:03:17 -0300

    adição do controller de getTools por tag

--- src/controllers/toolsController.js
+++ src/controllers/toolsController.js
@@ -1,10 +1,25 @@
 const toolsModels = require('../models/toolsModels');
 
-const getAllTools = async (_req, res) => {
-  const tools = await toolsModels.getAllTools();
-  return res.status(200).json(tools);
+const getTools = async (req, res) => {
+  try {
+    if (req.query.tag) {
+      const { tag } = req.query;
+      const tools = await toolsModels.getToolByTag(tag);
+
+      if (!tools.length) {
+        return res.status(404).json({ message: 'Nenhuma ferramenta encontrada para a tag fornecida.' });
+      };
+
+      return res.status(200).json(tools);
+    };
+
+    const tools = await toolsModels.getAllTools();
+    return res.status(200).json(tools);
+  } catch (error) {
+    return res.status(500).json({ message: 'Erro ao obter as ferramentas.' });
+  };
 };
 
 module.exports = {
-  getAllTools,
-}
+  getTools,
+};
