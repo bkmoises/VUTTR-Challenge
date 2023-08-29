cd722b3b568a6f7aa2295e16c32ccc7d936aff05
bkmoises <bk_moises@hotmail.com>
2023-08-29 19:24:23 -0300

    refatorado para deixar a validação dos campos mais enxuto

--- src/middlewares/toolsMiddlewares.js
+++ src/middlewares/toolsMiddlewares.js
@@ -1,58 +1,21 @@
-const validateFieldTitle = (req, res, next) => {
+const validateField = (fieldName) => (req, res, next) => {
   const { body } = req;
 
-  if (body.title === undefined) {
-    return res.status(400).json({ message: 'O campo title é requerido.' });
-  };
+  if (body[fieldName] === undefined) {
+    return res.status(400).json({ message: `O campo ${fieldName} é requerido.` });
+  }
 
-  if (!body.title) {
-    return res.status(400).json({ message: 'O campo title não pode ser vazio.' });
-  };
+  if (!body[fieldName]) {
+    return res.status(400).json({ message: `O campo ${fieldName} não pode ser vazio.` });
+  }
 
   next();
 };
 
-const validateFieldLink = (req, res, next) => {
-  const { body } = req;
-
-  if (body.link === undefined) {
-    return res.status(400).json({ message: 'O campo link é requerido.' });
-  };
-
-  if (!body.link) {
-    return res.status(400).json({ message: 'O campo link não pode ser vazio.' });
-  };
-
-  next();
-};
-
-const validateFieldDescription = (req, res, next) => {
-  const { body } = req;
-
-  if (body.description === undefined) {
-    return res.status(400).json({ message: 'O campo description é requerido.' });
-  };
-
-  if (!body.description) {
-    return res.status(400).json({ message: 'O campo description não pode ser vazio.' });
-  };
-
-  next();
-};
-
-const validateFieldTags = (req, res, next) => {
-  const { body } = req;
-
-  if (body.tags === undefined) {
-    return res.status(400).json({ message: 'O campo tags é requerido.' });
-  };
-
-  if (!body.tags) {
-    return res.status(400).json({ message: 'O campo tags não pode ser vazio.' });
-  };
-
-  next();
-};
+const validateFieldTitle = validateField('title');
+const validateFieldLink = validateField('link');
+const validateFieldDescription = validateField('description');
+const validateFieldTags = validateField('tags');
 
 module.exports = {
   validateFieldTitle,
