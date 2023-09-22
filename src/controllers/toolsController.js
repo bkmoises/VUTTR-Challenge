6ac3a9be0304cee9f6fb7e554821ff77fb678824
bkmoises <bk_moises@hotmail.com>
2023-09-22 11:52:30 -0300

    inclusão da documentaçao dos controllers

--- src/controllers/toolsController.js
+++ src/controllers/toolsController.js
@@ -1,5 +1,12 @@
 const toolsModels = require('../models/toolsModels');
 
+/**
+ * Retorna todas as ferramentas ou as ferramentas filtradas por uma tag.
+ *
+ * @param {Object} req - O objeto de requisição HTTP.
+ * @param {Object} res - O objeto de resposta HTTP.
+ * @returns {Object} - Retorna uma resposta JSON contendo as ferramentas encontradas ou uma mensagem de erro em caso de falha.
+ */
 const getTools = async (req, res) => {
   try {
     const { tag } = req.query;
@@ -9,7 +16,7 @@ const getTools = async (req, res) => {
         const tools = await toolsModels.getToolByTag(tag);
         return res.status(200).json(tools);
       } else {
-        return res.status(500).json({ message: 'O parâmetro informado é invalido!' });
+        return res.status(500).json({ message: 'O parâmetro informado é inválido!' });
       };
     }
 
@@ -21,6 +28,13 @@ const getTools = async (req, res) => {
   };
 };
 
+/**
+ * Cria uma nova ferramenta com base nos dados fornecidos no corpo da requisição.
+ *
+ * @param {Object} req - O objeto de requisição HTTP com os dados da nova ferramenta.
+ * @param {Object} res - O objeto de resposta HTTP.
+ * @returns {Object} - Retorna uma resposta JSON contendo a nova ferramenta criada ou uma mensagem de erro em caso de falha.
+ */
 const createTool = async (req, res) => {
   try {
     const createdTool = await toolsModels.createTool(req.body);
@@ -31,6 +45,13 @@ const createTool = async (req, res) => {
   };
 };
 
+/**
+ * Remove uma ferramenta com base no ID fornecido como parâmetro na URL.
+ *
+ * @param {Object} req - O objeto de requisição HTTP com o ID da ferramenta a ser removida.
+ * @param {Object} res - O objeto de resposta HTTP.
+ * @returns {Object} - Retorna uma resposta JSON vazia em caso de sucesso ou uma mensagem de erro em caso de falha.
+ */
 const deleteTool = async (req, res) => {
   const { id } = req.params;
 
@@ -42,6 +63,13 @@ const deleteTool = async (req, res) => {
   };
 };
 
+/**
+ * Atualiza uma ferramenta com base no ID fornecido como parâmetro na URL e nos dados fornecidos no corpo da requisição.
+ *
+ * @param {Object} req - O objeto de requisição HTTP com o ID da ferramenta a ser atualizada e os novos dados.
+ * @param {Object} res - O objeto de resposta HTTP.
+ * @returns {Object} - Retorna uma resposta JSON vazia em caso de sucesso ou uma mensagem de erro em caso de falha.
+ */
 const updateTool = async (req, res) => {
   const { id } = req.params;
 
