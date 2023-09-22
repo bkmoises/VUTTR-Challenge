f7691ae605220803b8170a16c1d286bedbd0e29f
bkmoises <bk_moises@hotmail.com>
2023-09-22 12:08:46 -0300

    inclusão da documentação do model de conexão com o banco de dados

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,6 +1,21 @@
+/**
+ * Módulo de Conexão com o Banco de Dados MySQL
+ *
+ * Este módulo cria uma conexão com o banco de dados MySQL utilizando o módulo e as variáveis de ambiente configuradas no arquivo .env.
+ *
+ **/
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
+/**
+ * Cria uma conexão com o banco de dados MySQL usando as variáveis de ambiente configuradas.
+ *
+ * @type {Pool}
+ * @property {string} host - O endereço do host do banco de dados MySQL.
+ * @property {string} user - O nome de usuário para autenticação no banco de dados.
+ * @property {string} password - A senha para autenticação no banco de dados.
+ * @property {string} database - O nome do banco de dados a ser usado.
+ */
 const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
